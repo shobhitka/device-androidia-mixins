73574c411ea28ab963df2cd16fac38d624a5575f
Shobhit Kumar <shobhit.kumar@intel.com>
2017-01-03 14:53:57 +0530

    FLASHFILES: Add needed files in BOARD_FLASHFILES
    This ensures that kernelflinger.efi and installer.efi are built and also
    needed scripts are copied
    
    Signed-off-by: Shobhit Kumar <shobhit.kumar@intel.com>

--- groups/boot-arch/android_ia/AndroidBoard.mk
+++ groups/boot-arch/android_ia/AndroidBoard.mk
@@ -4,6 +4,12 @@ endef
 
 out_flashfiles := $(PRODUCT_OUT)/$(TARGET_PRODUCT).flashfiles.$(TARGET_BUILD_VARIANT).$(USER).zip
 
+$(PRODUCT_OUT)/efi/installer.cmd:
+	$(ACP) $(TARGET_DEVICE_DIR)/$(@F) $@
+
+$(PRODUCT_OUT)/efi/startup.nsh:
+	$(ACP) $(TARGET_DEVICE_DIR)/$(@F) $@
+
 $(out_flashfiles): $(BOARD_FLASHFILES) | $(ACP)
 	$(call generate_flashfiles,$@, $^)
 
